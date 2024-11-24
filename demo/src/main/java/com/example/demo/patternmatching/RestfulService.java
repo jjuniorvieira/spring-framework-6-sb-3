82c233cf8da31c2065c6ecc658390b2346c38808
J. Junior <junior.jaava@gmail.com>
2024-11-24 16:51:54 +0100

    using jakarta objects
    Signed-off-by: J. Junior <junior.jaava@gmail.com>

--- demo/src/main/java/com/example/demo/patternmatching/RestfulService.java
+++ demo/src/main/java/com/example/demo/patternmatching/RestfulService.java
@@ -1,6 +1,17 @@
 package com.example.demo.patternmatching;
 
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@RestController
 public final class RestfulService implements WebService {
+
+    private final HttpServletRequest httpServletRequest;
+
+    public RestfulService(HttpServletRequest httpServletRequest) {
+        this.httpServletRequest = httpServletRequest;
+    }
 
     @Override
     public String execute(String body) {
